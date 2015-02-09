443c83a818cfe3736261b8ae2464d4b4246b8eb2
Jordan Collier <jordanicollier@gmail.com>
2015-02-09 09:56:23 -0700

    fix 06 exercise

--- 06_favorite_food.rb
+++ 06_favorite_food.rb
@@ -1,4 +1,6 @@
 require_relative "person"
 
 # Print Katy's favorite foods. It should read "Katy's favorite foods are sushi, hamburgers, and pho."
-puts "#{KATY_PERRY[:first_name]}'s favorite foods are #{KATY_PERRY[:favorite_foods].join(", ")}."
+foods = KATY_PERRY[:favorite_foods][0..-2].join(', ') + ', and ' + KATY_PERRY[:favorite_foods][-1]
+
+puts "#{KATY_PERRY[:first_name]}'s favorite foods are #{foods}."
