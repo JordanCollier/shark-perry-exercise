91b70f21acdbaf99b06ff22502bb31d50b102512
Jordan Collier <jordanicollier@gmail.com>
2015-02-09 09:45:33 -0700

    complete exercises

--- 06_favorite_food.rb
+++ 06_favorite_food.rb
@@ -1,3 +1,4 @@
 require_relative "person"
 
 # Print Katy's favorite foods. It should read "Katy's favorite foods are sushi, hamburgers, and pho."
+puts "#{KATY_PERRY[:first_name]}'s favorite foods are #{KATY_PERRY[:favorite_foods].join(", ")}."
